5776535ef489537b77730976c37f247e03981015
“JIHYUN” <791004_@likelion.org>
2019-02-28 12:14:37 +0900

    first one

--- sidlab/settings.py
+++ sidlab/settings.py
@@ -20,10 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v#b=dzdt*i!42jpa8_*-p#%y+k12g&0ev^_r06b0@2u!hdo7hc'
+# SECRET_KEY = 'v#b=dzdt*i!42jpa8_*-p#%y+k12g&0ev^_r06b0@2u!hdo7hc'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'v#b=dzdt*i!42jpa8_*-p#%y+k12g&0ev^_r06b0@2u!hdo7hc')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
