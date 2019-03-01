24602beeaaee71fa4c470a0fbb1f74be9e3af29a
“JIHYUN” <791004_@likelion.org>
2019-03-02 01:59:28 +0900

    css

--- sidlab/settings.py
+++ sidlab/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+STATIC_URL = 'https://s3.ap-northeast-2.amazonaws.com/sid-media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
